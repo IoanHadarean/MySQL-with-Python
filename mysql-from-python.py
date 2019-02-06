fbd26b4fa704384cd9faaa771e023b8c84bedf77
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-02-06 11:19:03 +0000

    Added more rows into Friends with executemany

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -15,8 +15,10 @@ connection = pymysql.connect(host = 'localhost',
 try:
     #Run a query
     with connection.cursor() as cursor:
-        row = ("Bob", 22, "1997-02-06 23:04:56")
-        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        rows = [("Bob", 22, "1997-02-06 23:04:56"),
+              ("Jim", 56, "1963-05-09 13:12:45"),
+              ("Fred", 100, "1919-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
         connection.commit();
 finally:
     # Close the connection, regardless of whether the 
